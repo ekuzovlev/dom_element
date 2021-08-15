1f33a4c3b367286525848deabfd3933449d3f021
kuz-en <bio19@ya.ru>
2021-08-15 12:58:00 +0300

    refactor

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ function DomElement(selector, height, width, bg, fontsize) {
   this.width = width;
   this.bg = bg;
   this.fontSize = fontsize;
+
   this.createElement = function () {
     let styles = `height          : ${this.height}px;
                   width: ${this.width}px;
                   background-color: ${this.bg};
                   font-size: ${this.fontSize}pt;`;
+
     if (this.selector.startsWith('.')) {
       let div = document.createElement('div');
       div.className = this.selector;
@@ -33,22 +35,3 @@ domElementClass.createElement();
 
 let domElementId = new DomElement('#best', 200, 300, 'blue', 30);
 domElementId.createElement();
-
-
-
-//domElement.createElement('#best');
-
-
-/* 1) Сделать класс DomElement, который
-   содержит свойства
-  - selector,
-  - height,
-  - width,
-  - bg,
-  - fontSize
-содержит метод, который создает элемент на странице в зависимости от условия:
-- если строка selector начинается с точки, создаем div с классом
-- если строка selector  начинается с решетки # то создаем параграф с id
-пример:
-если передана строка '.block', то функция конструктор создает элемент с class="block"
-если передана строка '#best', то функция конструктор создает элемент с id =best"  */
